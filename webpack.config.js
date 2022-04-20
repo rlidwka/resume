7e62d86a1ca5ae2d35b355fedfdc3c9b7af31fae
Alex Kocharin <alex@kocharin.ru>
2022-04-21 02:36:17 +0300

    Add all the stuff

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+
+export default (env) => ({
+  mode: env.WEBPACK_SERVE ? 'development' : 'production',
+  devtool: env.WEBPACK_SERVE ? 'eval-cheap-source-map' : undefined,
+  entry: [ './src/index.js' ],
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({ template: './src/index.pug' })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        use: [ 'pug-loader' ]
+      },
+      {
+        test: /\.css$/,
+        use: [ MiniCssExtractPlugin.loader, 'css-loader' ]
+      },
+      {
+        test: /\.styl$/,
+        exclude: /node_modules/,
+        use: [ MiniCssExtractPlugin.loader, 'css-loader', 'stylus-loader' ]
+      }
+    ]
+  },
+  devServer: {
+    watchFiles: {
+      paths: 'src/**',
+      options: {
+        ignore: '.*'
+      }
+    }
+  }
+})
